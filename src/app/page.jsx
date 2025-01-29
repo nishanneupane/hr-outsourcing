eea0e894cd646dae5214aaa75a7657a284bd26f8
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2025-01-29 08:38:48 +0545

    Update page.jsx 404

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -39,6 +39,14 @@ export default function Home() {
 
   return (
     <main className="relative">
+       <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-6xl font-bold text-gray-800">404</h1>
+      <h2 className="text-2xl font-semibold text-gray-600 mt-2">Page Not Found</h2>
+      <p className="text-gray-500 mt-4 text-center max-w-md">
+        Sorry, the page you are looking for does not exist. It might have been moved or deleted.
+      </p>
+      
+    </div>
 {/*       <Header />
       <Hero />
       <Details />
