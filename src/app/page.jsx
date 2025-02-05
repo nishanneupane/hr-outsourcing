2252a3bd3bd3004676e60bef6afe67d609d2dc31
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2025-02-05 13:22:21 +0545

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -38,15 +38,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="relative">
-       <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-gray-800">404</h1>
-      <h2 className="text-2xl font-semibold text-gray-600 mt-2">Page Not Found</h2>
-      <p className="text-gray-500 mt-4 text-center max-w-md">
-        Sorry, the page you are looking for does not exist. It might have been some issues.
-      </p>
-      
-    </div>
+   
 {/*       <Header />
       <Hero />
       <Details />
@@ -59,6 +51,6 @@ export default function Home() {
           <ContactForm />
         )}
       </AnimatePresence> */}
-    </main>
+    
   );
 }
