f41fa74865190bae198f4eb8f1128834f5a45b49
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2025-02-09 13:57:30 +0545

    Update cta.jsx

--- src/sections/cta.jsx
+++ src/sections/cta.jsx
@@ -88,7 +88,7 @@ const CTA = () => {
                     
                     </div>
                 </motion.div>
-                     <p className="text-sm bg-gray-900 text-white text-center py-4">
+                     <p className="text-sm bg-gray-900 text-white text-center py-10">
         © 2024 All rights reserved. Designed & Made by{" "}
         <a
           href="https://anish-10.vercel.app"
